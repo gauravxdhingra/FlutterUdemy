77783db0602f1351326f2613dfe93221eeb8813d
GauravXd <grvdhingra1999@gmail.com>
2019-11-11 22:43:13 +0530

    Resolving The Last Question Bug

--- lib/main.dart
+++ lib/main.dart
@@ -17,29 +17,38 @@ class MyAppState extends State<MyApp> {
   var _questionIndex = 0;
 
   void _answerQuestion() {
+    // var aBool = true;
+    // aBool = false;
+
     setState(() {
       _questionIndex++;
     });
     print(_questionIndex);
+    if (_questionIndex < ques.length) {
+      print('We have more questons');
+    } else
+      (print('No more questions'));
   }
+
+  final ques = const [
+    {
+      'questionSet': 'What\'s your name?',
+      'answerSet': ['Gaurav', 'Rishabh', 'Lakshay', 'Shubham'],
+    },
+    {
+      'questionSet': 'What\'s your age?',
+      'answerSet': ['10', '20', '30', '40'],
+    },
+    {
+      'questionSet': 'What\'s your favourite color?',
+      'answerSet': ['Black', 'Blue', 'Red', 'White'],
+    },
+  ];
+
+//questions=[] doesn't work if questions is const. try var questions = const[] in upper list.
 
   @override
   Widget build(BuildContext context) {
-    var ques = [
-      {
-        'questionSet': 'What\'s your name?',
-        'answerSet': ['Gaurav', 'Rishabh', 'Lakshay', 'Shubham'],
-      },
-      {
-        'questionSet': 'What\'s your age?',
-        'answerSet': ['10', '20', '30', '40'],
-      },
-      {
-        'questionSet': 'What\'s your Favourite Color?',
-        'answerSet': ['Black', 'Blue', 'Red', 'White'],
-      },
-    ];
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -49,17 +58,21 @@ class MyAppState extends State<MyApp> {
             textAlign: TextAlign.center,
           ),
         ),
-        body: Column(
-          children: <Widget>[
-            Questions(
-              ques[_questionIndex]['questionSet'],
-            ),
-            ...(ques[_questionIndex]['answerSet'] as List<String>)
-                .map((answer) {
-              return Answers(_answerQuestion, answer);
-            }).toList()
-          ],
-        ),
+        body: _questionIndex < ques.length
+            ? Column(
+                children: <Widget>[
+                  Questions(
+                    ques[_questionIndex]['questionSet'],
+                  ),
+                  ...(ques[_questionIndex]['answerSet'] as List<String>)
+                      .map((answer) {
+                    return Answers(_answerQuestion, answer);
+                  }).toList()
+                ],
+              )
+            : Center(
+                child: Text('You did it!'),
+              ),
       ),
     );
   }
